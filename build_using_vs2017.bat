877c32111b416c7bc3bc21d5dc38dc916a2efbc0
mancoast <RobertPancoast77@gmail.com>
2018-12-01 20:49:43 -0500

    Update build_using_vs2017.bat

--- build_using_vs2017.bat
+++ build_using_vs2017.bat
@@ -8,6 +8,7 @@ set _SCRIPT_FOLDER=%~dp0
 set INITDIR=%_SCRIPT_FOLDER%
 set SRC=%INITDIR%
 set BUILDTREE=%SRC%\build\
+set INCS=%SRC%\inc\
 SET tbs_arch=x86
 SET vcvar_arg=x86
 SET cmake_platform="Visual Studio 15 2017"
@@ -25,6 +26,11 @@ rd /s /q %BUILDTREE%
 mkdir %BUILDTREE%
 set BINDIR=%BUILDTREE%\bin\
 mkdir %BINDIR%
+
+REM # TODO: install required includes to %INCS%
+REM # https://github.com/KhronosGroup/EGL-Registry/tree/master/api/EGL #
+REM # https://github.com/KhronosGroup/EGL-Registry/tree/master/api/KHR #
+REM # https://github.com/KhronosGroup/OpenCL-Headers/tree/master/CL #
 
 %_SCRIPT_DRIVE%
 cd %BUILDTREE%
