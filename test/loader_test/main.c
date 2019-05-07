7d2584c4b8b1ac186f67143aaa2f846184709a9e
Ben Ashbaugh <ben.ashbaugh@intel.com>
2019-05-07 07:13:17 -0700

    Create directory tree, fix warnings, cmake improvements (#58)
    * fix make test
    
    * include CTest and wrap tests with BUILD_TESTING
    
    * move loader files to their own directory
    
    * create specific directories for Windows and Linux
    
    * cmake improvements
    
    * fix path to map file for linux build
    
    * add definition for CL_TARGET_OPENCL_VERSION
    
    * fix warnings
    
    * don't use target_link_options for cmake compatibility
    
    * remove SHARED from a few cmake files

--- test/loader_test/main.c
+++ test/loader_test/main.c
@@ -11,11 +11,10 @@ extern int test_buffer_object();
 extern int test_program_objects();
 extern int test_image_objects();
 extern int test_sampler_objects();
-extern int initialize_log();
-extern int test_icd_match();
-
 extern int test_OpenGL_share();
-extern int test_Direct3D10_share();
+extern int test_release_calls();
+
+extern int test_icd_match();
 
 int main(int argc, char **argv)
 {
@@ -31,9 +30,8 @@ int main(int argc, char **argv)
     test_image_objects();
     test_sampler_objects();
     test_OpenGL_share();
-
-//    test_Direct3D10_share();
     test_release_calls();
+
     test_icd_close_app_log();
     test_icd_close_stub_log();
     
