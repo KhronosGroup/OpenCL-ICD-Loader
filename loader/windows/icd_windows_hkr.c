079803a065fe5f3e4988d96538d0fadea778e211
saleelk <saleel@asu.edu>
2019-06-14 00:47:55 -0700

    Update regkey query method

--- loader/windows/icd_windows_hkr.c
+++ loader/windows/icd_windows_hkr.c
@@ -117,7 +117,7 @@ static bool ReadOpenCLKey(DEVINST dnDevNode)
 
         result = RegQueryValueExA(
             hkey,
-            OPENCL_REG_SUB_KEY,
+            GetOpenCLRegKeyName(),
             NULL,
             &dwLibraryNameType,
             (LPBYTE)cszOclPath,
