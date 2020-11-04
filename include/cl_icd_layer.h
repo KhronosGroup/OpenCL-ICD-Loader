70829370b866887d97a66d3d2f415f5f40eee266
Brice Videau <bvideau@anl.gov>
2020-11-04 11:37:27 -0600

    Added formal definition of Layer API entry points.

--- include/cl_icd_layer.h
+++ include/cl_icd_layer.h
@@ -31,17 +31,29 @@ typedef cl_uint cl_layer_api_version;
 #define CL_LAYER_API_VERSION 0x0001
 #define CL_LAYER_API_VERSION_100 100
 
-CL_API_ENTRY typedef cl_int (CL_API_CALL *pfn_clGetLayerInfo)(
-    cl_layer_info  param_name,
-    size_t         param_value_size,
-    void          *param_value,
-    size_t        *param_value_size_ret);
+extern CL_API_ENTRY cl_int CL_API_CALL
+clGetLayerInfo(cl_layer_info  param_name,
+               size_t         param_value_size,
+               void          *param_value,
+               size_t        *param_value_size_ret);
 
-CL_API_ENTRY typedef cl_int (CL_API_CALL *pfn_clInitLayer)(
-    cl_uint                         num_entries,
-    const struct _cl_icd_dispatch  *target_dispatch,
-    cl_uint                        *num_entries_ret,
-    const struct _cl_icd_dispatch **layer_dispatch);
+CL_API_ENTRY typedef cl_int
+(CL_API_CALL *pfn_clGetLayerInfo)(cl_layer_info  param_name,
+                                  size_t         param_value_size,
+                                  void          *param_value,
+                                  size_t        *param_value_size_ret);
+
+extern CL_API_ENTRY cl_int CL_API_CALL
+clInitLayer(cl_uint                         num_entries,
+            const struct _cl_icd_dispatch  *target_dispatch,
+            cl_uint                        *num_entries_ret,
+            const struct _cl_icd_dispatch **layer_dispatch);
+
+CL_API_ENTRY typedef cl_int
+(CL_API_CALL *pfn_clInitLayer)(cl_uint                         num_entries,
+                               const struct _cl_icd_dispatch  *target_dispatch,
+                               cl_uint                        *num_entries_ret,
+                               const struct _cl_icd_dispatch **layer_dispatch);
 
 #ifdef __cplusplus
 }
