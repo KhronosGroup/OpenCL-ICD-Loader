5f8249691ec8c25775789498951f8e9eb62c201d
Kévin Petit <kpet@free.fr>
2021-02-22 05:24:55 +0000

    Replace uses of CL_EXT_{PRE,SUF}FIX* with CL_API_{PRE,SUF}FIX* (#142)
    Generation done using https://github.com/KhronosGroup/OpenCL-Docs/pull/569
    
    Signed-off-by: Kévin Petit <kpet@free.fr>

--- test/driver_stub/cl.c
+++ test/driver_stub/cl.c
@@ -398,7 +398,7 @@ CL_API_ENTRY cl_int CL_API_CALL
 clSetCommandQueueProperty(cl_command_queue               command_queue ,
                             cl_command_queue_properties    properties ,
                             cl_bool                        enable ,
-                            cl_command_queue_properties *  old_properties) CL_EXT_SUFFIX__VERSION_1_0_DEPRECATED
+                            cl_command_queue_properties *  old_properties) CL_API_SUFFIX__VERSION_1_0_DEPRECATED
 {
     cl_int return_value = CL_OUT_OF_RESOURCES;
     test_icd_stub_log("clSetCommandQueueProperty(%p, %p, %u, %p)\n",
--- test/driver_stub/cl_gl.c
+++ test/driver_stub/cl_gl.c
@@ -207,7 +207,7 @@ clGetGLContextInfoKHR(const cl_context_properties *  properties,
 CL_API_ENTRY cl_event CL_API_CALL
 clCreateEventFromGLsyncKHR(cl_context            context ,
                            cl_GLsync             cl_GLsync ,
-                           cl_int *              errcode_ret ) CL_EXT_SUFFIX__VERSION_1_1
+                           cl_int *              errcode_ret ) CL_API_SUFFIX__VERSION_1_1
 
 {
      cl_event ret_event = (cl_event)(SIZE_T_MAX);
