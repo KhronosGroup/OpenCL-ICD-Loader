a7cdc5352e227fc27073e175faea3a0447cf3247
Ben Ashbaugh <ben.ashbaugh@intel.com>
2022-10-07 23:47:03 -0700

    add back the def file

--- loader/icd_platform.h
+++ loader/icd_platform.h
@@ -40,18 +40,4 @@
 #error Unknown OS!
 #endif
 
-#if defined _WIN32 || defined __CYGWIN__
-    #ifdef __GNUC__
-        #define CL_API_ENTRY __attribute__((dllexport))
-    #else
-        #define CL_API_ENTRY __declspec(dllexport)
-    #endif
-#else
-    #if __GNUC__ >= 4
-        #define CL_API_ENTRY __attribute__((visibility("default")))
-    #else
-        #define CL_API_ENTRY
-    #endif
-#endif
-
 #endif
