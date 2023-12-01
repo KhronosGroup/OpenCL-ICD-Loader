b7c7089231dc9b9a23a75abc81a3cbaba8cd5a0f
Brice Videau <bvideau@anl.gov>
2023-11-30 19:16:15 -0600

    Add instance support.

--- include/cl_khr_icd2.h
+++ include/cl_khr_icd2.h
@@ -34,3 +34,51 @@ clIcdSetPlatformDispatchDataKHR_t(
 typedef clIcdSetPlatformDispatchDataKHR_t *
 clIcdSetPlatformDispatchDataKHR_fn;
 #endif // !defined(CL_ICD2_TAG_KHR)
+
+#if !defined(CL_INVALID_INSTANCE_KHR)
+#define CL_INVALID_INSTANCE_KHR -1154
+
+typedef struct _cl_instance *cl_instance;
+typedef cl_properties cl_instance_properties;
+
+typedef cl_instance CL_API_CALL
+clCreateInstanceKHR_t(
+    const cl_instance_properties *properties,
+    cl_int *errcode_ret);
+
+typedef clCreateInstanceKHR_t *
+clCreateInstanceKHR_fn;
+
+typedef cl_int CL_API_CALL
+clDestroyInstanceKHR_t(
+    cl_instance instance);
+
+typedef clDestroyInstanceKHR_t *
+clDestroyInstanceKHR_fn;
+
+typedef cl_int CL_API_CALL
+clGetPlatformIDsForInstanceKHR_t(
+    cl_instance instance,
+    cl_uint num_entries,
+    cl_platform_id *platforms,
+    cl_uint *num_platforms);
+
+typedef clGetPlatformIDsForInstanceKHR_t *
+clGetPlatformIDsForInstanceKHR_fn;
+
+typedef cl_platform_id CL_API_CALL
+clIcdCreateInstancePlatformKHR_t(
+    cl_platform_id platform,
+    cl_int *errcode_ret);
+
+typedef clIcdCreateInstancePlatformKHR_t *
+clIcdCreateInstancePlatformKHR_fn;
+
+typedef cl_int CL_API_CALL
+clIcdDestroyInstancePlatformKHR_t(
+    cl_platform_id platform);
+
+typedef clIcdDestroyInstancePlatformKHR_t *
+clIcdDestroyInstancePlatformKHR_fn;
+
+#endif // !defined(CL_INVALID_INSTANCE_KHR)
