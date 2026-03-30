43295b9470ccf3fbe0c92557e0bfadf46a0edf76
Pino Toscano <toscano.pino@tiscali.it>
2026-03-30 08:53:32 +0200

    feat: add support for GNU/Hurd
    Nothing else is needed beside using the bits of other Unix OSes.

--- loader/icd_platform.h
+++ loader/icd_platform.h
@@ -19,7 +19,7 @@
 #ifndef _ICD_PLATFORM_H_
 #define _ICD_PLATFORM_H_
 
-#if defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__QNXNTO__)
+#if defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__QNXNTO__) || defined(__GNU__)
 
 #define PATH_SEPARATOR  ':'
 #define DIRECTORY_SYMBOL '/'
